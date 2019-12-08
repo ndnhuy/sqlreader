5061554269192eff9702dbb2d5178068f98c6827
huy.n <huy.n@antfin.com>
2019-12-08 18:25:38 +0700

    codemirror

--- src/main/java/icashiercore/ConfigController.java
+++ src/main/java/icashiercore/ConfigController.java
@@ -2,13 +2,16 @@ package icashiercore;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class ConfigController {
 
     @Autowired
-    private ConfigRepository configRepository;
+    private ConfigService configService;
 
     @GetMapping("/config")
     public String index() {
@@ -16,7 +19,12 @@ public class ConfigController {
     }
 
     @GetMapping("/all")
-    public String getAll() {
-        return configRepository.getAll().toString();
+    public Map<String, String> getAll() {
+        return configService.getAll();
+    }
+
+    @GetMapping("/search")
+    public Map<String, String> search(@RequestParam("text") String text) {
+        return configService.search(text);
     }
 }
--- src/main/java/icashiercore/ConfigService.java
+++ src/main/java/icashiercore/ConfigService.java
@@ -1,5 +1,6 @@
 package icashiercore;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
@@ -8,18 +9,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
-public class ConfigRepository {
+@JsonPropertyOrder
+public class ConfigService {
 
     private static Set<String> TABLE_NAMES = new HashSet<>(Arrays.asList("ipd_channel_decision_rule"));
 
-    public JSONObject getAll() {
+    public Map<String, String> getAll() {
         try {
             String repoPath = "/Users/administrator/huynguyen/repos/icashiercoreConf";
             List<Path> tablePaths = Files.list(Paths.get(repoPath))
@@ -27,15 +26,33 @@ public class ConfigRepository {
                 .filter(path -> TABLE_NAMES.stream().anyMatch(tableName -> path.toString().endsWith(tableName + ".json")))
                 .collect(Collectors.toList());
 
-            JSONObject result = new JSONObject();
+            Map<String, String> result = new LinkedHashMap<>();
             for (Path p : tablePaths) {
                 String content = new String(Files.readAllBytes(p));
-                result.put(p.getFileName().toString(), new JSONArray(content));
+                result.put(p.getFileName().toString(), content);
             }
 
             return result;
         } catch (IOException ex) {
             throw new RuntimeException("Error while reading files");
         }
+    }
+
+    public Map<String, String> search(String text) {
+        Map<String, String> all = getAll();
+        return all.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> newContent(entry.getValue(), text)));
+    }
+
+    private String newContent(String content, String searchText) {
+        JSONArray arr = new JSONArray(content);
+        JSONArray newArr = new JSONArray();
+        for (Object o : arr) {
+            JSONObject row = (JSONObject) o;
+            if (row.toString().toUpperCase().contains(searchText.toUpperCase())) {
+                newArr.put(row);
+            }
+        }
+        return newArr.toString();
     }
 }
